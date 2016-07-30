63f3fc565f5bd4dcfab5d3e0d59acf6e5ab0d1ae
Carlo Refice <CRefice@gmail.com(none)>
2016-07-30 14:26:16 +0200

    Added possibility to open .ttp files with the windows context menu

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using TranslaTale.Projects;
+using TranslaTale.Settings;
+
 namespace TranslaTale
 {
     static class Program
@@ -11,12 +16,24 @@ namespace TranslaTale
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            new StartUpForm().Show(); 
+            if (args.Count() > 0 &&
+                File.Exists(args[0]) &&
+                Path.GetExtension(args[0]) == ".ttp")
+            {
+                var mainForm = new MainForm();
+                mainForm.Open(Project.Read(args[0]));
+                mainForm.Show();
+            }
+            else
+            {
+                new StartUpForm().Show();
+            }
+
             Application.Run();
         }
     }
